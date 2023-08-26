adb2a31c084afde271c5434c10ed2e5ace660819
Shashi Kumar Singh <shashikumarsingh686@gmail.com>
2023-08-26 13:01:25 +0530

    changing image name

--- src/component/ui/Header.js
+++ src/component/ui/Header.js
@@ -4,7 +4,7 @@ import got from '../../img/got.png';
 const Header = () => {
   return (
     <header className='center'>
-        <img src = {got} alt = 'breaking bad' height={120} width={100}/>
+        <img src = {got} alt = 'game of thrones' height={120} width={100}/>
     </header>
   )
 }
